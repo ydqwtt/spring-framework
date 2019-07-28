91668616f7c4b5d6e1033fb72c3e9a522e4ad372
ydqwtt <279168392@qq.com>
2019-07-28 14:36:27 +0800

    自测

--- yunsign/src/main/java/com/yunsign/Test.java
+++ yunsign/src/main/java/com/yunsign/Test.java
@@ -2,10 +2,8 @@ package com.yunsign;
 
 import com.yunsign.config.Appconfig;
 import com.yunsign.dao.Dao;
-import com.yunsign.dao.Indexdao;
+import com.yunsign.dao.IndexDao;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import java.sql.Wrapper;
 
 public class Test {
 	public static void main(String[] args) {
@@ -13,8 +11,8 @@ public class Test {
 		annotationConfigApplicationContext.register(Appconfig.class);
 		annotationConfigApplicationContext.refresh();
 
-		Dao indexDao = annotationConfigApplicationContext.getBean(Indexdao.class);
-		Dao indexDao2 = annotationConfigApplicationContext.getBean(Indexdao.class);
+		Dao indexDao = annotationConfigApplicationContext.getBean(IndexDao.class);
+		Dao indexDao2 = annotationConfigApplicationContext.getBean(IndexDao.class);
 		System.out.println(indexDao.hashCode());
 		System.out.println(indexDao2.hashCode());
 		indexDao.query();
--- yunsign/src/main/java/com/yunsign/wrapper/MyBeanPostProcessor.java
+++ yunsign/src/main/java/com/yunsign/wrapper/MyBeanPostProcessor.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MyBeanPostProcessor implements BeanFactoryPostProcessor {
 	@Override
 	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-		AnnotatedBeanDefinition annotatedBeanDefinition = (AnnotatedBeanDefinition) beanFactory.getBeanDefinition("IndexDao");
+		AnnotatedBeanDefinition annotatedBeanDefinition = (AnnotatedBeanDefinition) beanFactory.getBeanDefinition("indexDao");
 		annotatedBeanDefinition.setScope("prototype");
 	}
 }
